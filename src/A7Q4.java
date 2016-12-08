69f0b8829fb5c245c03c4b701ea281c1ae04f18e
micla1676 <micla1676@HRH-IC0027336.wrdsb.ca>
2016-12-08 10:03:02 -0500

    Assignment 7 complete

--- src/A7Q4.java
+++ src/A7Q4.java
@@ -16,37 +16,37 @@ public class A7Q4 {
         Scanner input = new Scanner(System.in);
 
         //create an array to store 10 numbers
-        int[] tenInt = new int[10];
+        int[] marks = new int[10];
         //create a for loop that loops twice
         for (int x = 0; x < 10; x = x + 1) {
             //ask for the 2 integers
-            System.out.println("please enter your integer:");
+            System.out.println("please enter your marks:");
             //store user input in the array
-            tenInt[x] = input.nextInt();
+            marks[x] = input.nextInt();
         }
-        //sort the 10 numbers
-        //loop 10 times
+        //sort the numbers
+        //loop a amount of times
         for (int x = 0; x < 10; x = x + 1) {
-            //loop 10 times
+            //loop a times
             for (int z = x + 1; z < 10; z = z + 1) {
                 //test if y is larger then the integer at index z
-                if (tenInt[x] > tenInt[z]) {
+                if (marks[x] > marks[z]) {
                     //if true, switch the 2 numbers
                     //create a temp storage for index z
-                    int temp = tenInt[z];
+                    int temp = marks[z];
                     //set index z equal to index x
-                    tenInt[z] = tenInt[x];
+                    marks[z] = marks[x];
                     //set index x equal to temp.
-                    tenInt[x] = temp;
+                    marks[x] = temp;
                 }
             }
         }
         //print out the 2 numbers
-        System.out.println("The integers in adcending order are: ");
+        System.out.println("The marks in adcending order are: ");
         //create a for loop that loops twice
         for (int x = 0; x < 10; x = x + 1) {
             //print out the 2 numbers
-            System.out.println(tenInt[x]);
+            System.out.println(marks[x]);
         }
     }
 }
--- src/A7Q6.java
+++ src/A7Q6.java
@@ -1,18 +1,17 @@
 
 import java.util.Scanner;
 
-/**
- * A7Q5
- *
+/**A7Q6
+ * find the lowest mark, highest mark, and average mark, in a nice way
  * @author micla1676
  */
-public class A7Q5 {
+public class A7Q6 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //create a scanner
+      //create a scanner
         Scanner input = new Scanner(System.in);
 
         //ask user for total number of marks
@@ -31,9 +30,9 @@ public class A7Q5 {
             marks[x] = input.nextInt();
         }
         //sort the numbers
-        //loop a amount of times
+        //loop a times
         for (int x = 0; x < a; x = x + 1) {
-            //loop a amount of times times
+            //loop a times
             for (int z = x + 1; z < a; z = z + 1) {
                 //test if y is larger then the integer at index z
                 if (marks[x] > marks[z]) {
@@ -47,38 +46,27 @@ public class A7Q5 {
                 }
             }
         }
-        //print out the 2 numbers
-        System.out.println("The marks in adcending order are:");
-        //create a for loop that loops twice
+        
+        //print out the lowest mark
+        System.out.println("The lowest mark is: " + marks[0]);
+        
+        //print out the highest mark
+        System.out.println("The highest mark is: " + marks[a-1]);
+        
+        //create an integer to store the average
+            double average = 0;
+        
+        //print out the average
         for (int x = 0; x < a; x = x + 1) {
-            //print out the 2 numbers
-            System.out.println(marks[x]);
+            //add the number at index x to the average
+            average = average + marks[x];
         }
-        //create double to store median
-        double total = 0;
-        //check if int a is divisable by 2
-        //create variable to store answer
-        int r = a % 2;
-
-        //if a is even
-        if (r == 0) {
-            //divide a by 2
-            a = a / 2;
-            //create a second variable to get the index value of a - 1
-            int z = a - 1;
-            //get the average
-            total = marks[a] + marks[z];
-            //divide by 2
-            total = total / 2;
-        } //if a is odd
-        else if (r > 0) {
-            //divide a by 2
-            //create integer to store a/2
-            int z = a / 2;
-            //set total equal to index of z
-            total = marks[z];
+        //find the average by dividing "average" by a
+        average = average / a;
+        
+        //print out the average
+        System.out.println("The average mark is: " + average);
+       
         }
-        //print out the median
-        System.out.println("The median is " + total);
+        
     }
-}
